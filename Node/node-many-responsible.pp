11736d77a7fd404cd7223e0cfdbf88e854653d55
BraunTom <38437248+BraunTom@users.noreply.github.com>
2019-02-27 12:27:59 +0100

    Delete node-many-responsible.pp

--- Node/node-many-responsible.pp
+++
@@ -1,7 +0,0 @@
-class node_many-responsible {
-        $admins = ["Admin"]
-        $users = ["Admin"]
-
-        realize(Accounts::Virtual[$admins], Accounts::Sudoroot[$admins])
-        realize(Accounts::Virtual[$users])
-}
