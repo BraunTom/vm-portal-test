4c98dcd56cdc047d45e4c84d715ce7164dc2960e
brauntom2@t-online.de <brauntom2@t-online.de>
2019-03-18 21:21:57 +0100

    Add M Mustermann

--- init.pp
+++ init.pp
@@ -1,4 +1,34 @@
-@accounts::virtual { 'M.Mustermann}':
+class accounts {
+  
+    @accounts::virtual { 'Max.Mustermann}':
+  uid             =>  4000,
+  realname        =>  'Max} #{Mustermann',
+  sshkeytype      =>  'ssh-rsa',
+  sshkey          =>  ''
+}
+  
+    @accounts::virtual { 'Maxe.Mustermann}':
+  uid             =>  4001,
+  realname        =>  'Maxe} #{Mustermann',
+  sshkeytype      =>  'ssh-rsa',
+  sshkey          =>  ''
+}
+  
+    @accounts::virtual { 'Maxee.Mustermann}':
+  uid             =>  4002,
+  realname        =>  'Maxee} #{Mustermann',
+  sshkeytype      =>  'ssh-rsa',
+  sshkey          =>  ''
+}
+  
+    @accounts::virtual { 'Ma.Mustermann}':
+  uid             =>  4003,
+  realname        =>  'Ma} #{Mustermann',
+  sshkeytype      =>  'ssh-rsa',
+  sshkey          =>  ''
+}
+  
+    @accounts::virtual { 'M.Mustermann}':
   uid             =>  4004,
   realname        =>  'M} #{Mustermann',
   sshkeytype      =>  'ssh-rsa',
