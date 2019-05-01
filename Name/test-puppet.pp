830cc8d8ce2c8d844ef5fb8769fe2ff4d4d06761
brauntom2@t-online.de <brauntom2@t-online.de>
2019-05-01 02:21:11 +0200

    Update test-puppet

--- Name/test-puppet.pp
+++ Name/test-puppet.pp
@@ -1,5 +1,5 @@
-node \'Puppetscript\'{
-  if defined( node_Puppetscript) {
-    class { node_Puppetscript}: }
+node 'test-puppet' {
+  if defined( node_test_puppet ) {
+    class { node_test_puppet: }
   }
 }
